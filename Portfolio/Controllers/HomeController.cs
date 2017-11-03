89203e0c61f85835a323dc8ad16460d8a04699d8
Jesse Bryan <jesse.bryan22@gmail.com>
2017-11-03 16:01:45 -0700

    Initial commit

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            var starredRepos = Github.GetStarredRepos();
+            return View(starredRepos);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portfolio/Models/Github.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class Github
+    {
+        public string Name { get; set; }
+        public string Html_url { get; set; }
+        public string Language { get; set; }
+        public int Stargazers_count { get; set; }
+
+        public static IEnumerable<Github> GetStarredRepos()
+        {
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("search/repositories?q=user:jbryan22&stars>=1&per_page=3");
+            //request.AddParameter("q", "user:jbryan22&stars>=1&per_page=3");
+            request.AddHeader("User-Agent", "jbryan22");
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var starredRepos = JsonConvert.DeserializeObject<List<Github>>(jsonResponse["items"].ToString())
+                .OrderByDescending(x => x.Stargazers_count);
+
+            return starredRepos;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
